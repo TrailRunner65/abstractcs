cc68b61a34b59129bc6f62114091f5affdc3f07b
TrailRunner65 <jwtempleton65@gmail.com>
2014-04-18 11:56:22 +1000

    Included UI- Bootstrap

--- js/common.js
+++ js/common.js
@@ -1,7 +1,19 @@
-var App = angular.module('App', []);
+var App = angular.module('App', ['ui.bootstrap']);
+//angular.module('myModule', ['ui.bootstrap']);
 
 App.controller('loadContent', function($scope, $http) {
 	$http.get('content/content.json').then(function(res){
     	$scope.content = res.data;
     });
+
+    $scope.groups = [
+        {
+            title: "Pace Calculator",
+            content: "Dynamic Group Body - 1"
+        },
+        {
+            title: "Race time Calculator",
+            content: "Dynamic Group Body - 2"
+        }
+    ];
 });
