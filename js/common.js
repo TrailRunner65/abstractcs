075e6d0b5f3989d44bd57d253434537ed1057f94
TrailRunner65 <jwtempleton65@gmail.com>
2014-04-30 19:41:26 +1000

    Added inital Books page

--- js/common.js
+++ js/common.js
@@ -4,15 +4,4 @@ App.controller('loadContent', function($scope, $http) {
 	$http.get('content/content.json').then(function(res){
     	$scope.content = res.data;
     });
-
-    $scope.groups = [
-        {
-            title: "Pace Calculator",
-            content: "Dynamic Group Body - 1"
-        },
-        {
-            title: "Race time Calculator",
-            content: "Dynamic Group Body - 2"
-        }
-    ];
 });
